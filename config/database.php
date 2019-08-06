e8d9c95d4c86bd937fd0427e234591b801dd89dd
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2019-08-06 14:59:23 +0900

    Set wordpress constants

--- config/database.php
+++ config/database.php
@@ -6,11 +6,10 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule;
 $capsule->addConnection([
     'driver'    => 'mysql',
-    'host'      => '192.168.95.100',
-    'database'  => 'local',
-    'username'  => 'root',
-    'password'  => 'root',
-    'port'      => '4002',
+    'host'      => DB_HOST,
+    'database'  => DB_NAME,
+    'username'  => DB_USER,
+    'password'  => DB_PASSWORD,
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => 'wp_',
