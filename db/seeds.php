e24f6f33928a9a7f62188d5ce3028ac4de7cc5a6
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2019-08-09 02:05:36 +0900

    Set initial post_has_the_term, default: true

--- db/seeds.php
+++ db/seeds.php
@@ -1,6 +1,10 @@
 <?php
   class Seed {
     public static function import() {
+      $post_has_the_term = get_site_option("post_has_the_term", null);
+      if (is_null($post_has_the_term)) {
+        add_site_option("post_has_the_term", 1);
+      }
     }
   }
 ?>
