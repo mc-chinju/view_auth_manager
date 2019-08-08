8b91f890ce5d84975b1a4907bd6a540bf85cd97a
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2019-08-08 19:14:17 +0900

    restrict post access

--- view_auth_manager.php
+++ view_auth_manager.php
@@ -14,6 +14,8 @@
   require_once(dirname(__FILE__) . "/db/seeds.php");
   require_once(dirname(__FILE__) . "/config/database.php");
 
+  use Illuminate\Database\Capsule\Manager as Capsule;
+
   function migrate() {
     CreateProgresses::change();
   }
@@ -41,8 +43,36 @@
     wp_enqueue_style( "menu_page", plugins_url( "style/menu_page.css", __FILE__ ) );
   }
 
+  function before_action_show_post() {
+    if (is_singular()) {
+      $capsule = new Capsule;
+
+      $current_user = wp_get_current_user();
+      $post_id = get_the_ID();
+
+      $view_auth_level_postmeta = $capsule::table("postmeta")->where("post_id", $post_id)->where("meta_key", "view_auth_level")->first();
+      $view_auth_level = $view_auth_level_postmeta ? $view_auth_level_postmeta->meta_value : 0;
+
+      $view_auth_term_id_postmeta = $capsule::table("postmeta")->where("post_id", $post_id)->where("meta_key", "view_auth_term_id")->first();
+      $view_auth_term_id = $view_auth_term_id_postmeta ? $view_auth_term_id_postmeta->meta_value : null;
+
+      $progress = $capsule::table("vam_progresses")->where("user_id", $current_user->ID)->where("term_id", $view_auth_term_id)->first();
+      $progress_level = $progress->level ?: 0;
+
+      // readable check
+      if ((is_null($view_auth_term_id)) || ($progress_level >= $view_auth_level)) {
+        // Noop
+      } else {
+        // TODO: Access accessable term_id's latest post
+        wp_safe_redirect(home_url(), 301);
+        exit;
+      }
+    }
+  }
+
   register_activation_hook( __FILE__, "migrate" );
   register_activation_hook( __FILE__, "seed" );
   register_deactivation_hook( __FILE__, "drop" );
   add_action( "admin_menu", "add_plugin_admin_menu" );
+  add_action( "template_redirect", "before_action_show_post" );
 ?>
